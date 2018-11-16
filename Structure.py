e732ca68e48151fb7b50ec01530ab71ce3ce24d4
Zachary Yamaoka <zach.yamaoka@gmail.com>
2018-11-16 13:07:36 +0000

    Preparing to Branch for show

--- Structure.py
+++ Structure.py
@@ -212,7 +212,7 @@ class Structure():
        #               [0, 1, 0, 1, 1, 0]])
 
        C[C<0.5] = 0
-       L[C>=0.5] = 5 # intial wire length
+       L[C>=0.5] = 1 # intial wire length
        C[C>=0.5] = 1 # Spring Constant
 
        C[np.eye(self.numElements*2)==1] = 0
