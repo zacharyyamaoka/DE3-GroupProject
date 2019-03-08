80a898ea107c70da5ba98ea2b40ba5be1b008ea7
Zachary Yamaoka <zach.yamaoka@gmail.com>
2019-03-08 07:33:25 +0000

    yaml drone

--- main/solve_fusion.py
+++ main/solve_fusion.py
@@ -17,11 +17,12 @@ Debugger = Debugger()
 plt.ion()
 K_s = 15000
 K_e = 19*10
-K, L, X = loadFusionStructure("drone_v4",strut_L = 0.55, strut_K=K_s, elastic_K=K_e, override_L=True)
-K, L, X = find_stability(K, L, X, Debugger, display_time=0, step=0.00001, iter = 100000)
+K, L, X = loadFusionStructure("drone_v4",strut_L = 0.44, strut_K=K_s, elastic_K=K_e, override_L=True)
+K, L, X = find_stability(K, L, X, Debugger, display_time=0, step=0.00001, iter = 50000)
 print("done")
 save_fusion360(X, K)
 overwrite_fusion360_file("drone_v4",X,K,L,K_s,K_e)
-
+save_YAML(X,K,"drone_v22")
+save_DROP_YAML("drone_v22",translation = [0, 50, 0]) # dm
 
 # strut_K = 500, elastic_K = 50, strut_L = 0.3, elastic_L = 0
